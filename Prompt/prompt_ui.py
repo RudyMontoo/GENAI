4e5f606cefebcf746a4057a9d0c5981e1534014a
RudyMontoo <rudraeng27@gmail.com>
2026-04-06 07:11:12 +0530

    study about implementation of prompt template

---
+++ Prompt/prompt_ui.py
@@ -0,0 +1,46 @@
+import os
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import streamlit as st
+# Load environment variables from .env file
+load_dotenv(dotenv_path="/home/rudra/dev/learning/GENAI/campusGEN/.env")  # or absolute path
+
+
+model = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    google_api_key=os.getenv("GOOGLE_API_KEY")
+)
+
+# st.header("Researcher Tool")
+
+# user_input=st.text_input("Enter your prompt")
+
+
+# if st.button("Summary"):
+#     result=model.invoke(user_input)
+#     st.write(result.content)
+
+
+
+
+from langchain_core.prompts import PromptTemplate,load_prompt
+st.header('Reasearch Tool')
+
+paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
+
+style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "Technical", "Code-Oriented", "Mathematical"] ) 
+
+length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
+
+template = load_prompt('template.json')
+
+
+
+if st.button('Summarize'):
+    chain = template | model
+    result = chain.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
+    st.write(result.content)
